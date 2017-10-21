39fa4ca0eafcaa335b5f4249480a26e879073dd1
Alexey Leshchuk <kuelye@gmail.com>
2017-10-22 00:47:45 +0300

    init project

---
+++ src/com/banana/I02/homework/Main.java
@@ -0,0 +1,11 @@
+package com.banana.I02.homework;
+
+import static com.banana.I02.homework.HiddenStorage.getSecretMessage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getSecretMessage());
+    }
+
+}
